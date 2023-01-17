5355e61bf38d9b26927c5f24bbe406fbcd6f0510
Talgat Tokayev <59330081+Talgat1164@users.noreply.github.com>
2023-01-18 00:40:21 +0600

    Thunk Middleware и Redux Dev Tools

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
 import { rootReducer } from "./redux/rootReducer";
 import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 
-const store = createStore(rootReducer);
-
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
 ReactDOM.render(
   <Provider store={store}>
     <App />
